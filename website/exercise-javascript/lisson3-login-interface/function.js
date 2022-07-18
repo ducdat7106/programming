78413d4ceafbff7930396a1dec569dc3749d299c
ducdat7106 <nhiennhienbn@gmail.com>
2022-07-18 21:34:23 +0700

    login update

--- website/exercise-javascript/lisson3-login-interface/function.js
+++ website/exercise-javascript/lisson3-login-interface/function.js
@@ -1,28 +1,19 @@
-function validate() 
-{
+function validate() {
+
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     if (username == "dat" && password == "0") {
-        
-        location.href = "fix.html";
-        // location.href = "https://www.w3schools.com";
+
+        // Redirecting to other page.
+        window.location.href = "login_successful.html";
 
         alert("Login successfully.Please continue.");
-
-        // return false;
-        // notification
-
 
     } else {
 
         // Redirecting to other page.
-        window.location.href = "index.html";
-        // window.location.href = "fix.html";
-
-        // return false;
-        // notification
-        //  alert("Login successfully.Please continue.");
+        window.location.href = "login_error.html";
 
         alert("Login failed. You need login again!")
     }
