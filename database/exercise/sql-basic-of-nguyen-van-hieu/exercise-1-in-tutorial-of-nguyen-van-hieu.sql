c6432170c5f3cfc8801cef8277968d2c09a77653
ducdat7106 <nhiennhienbn@gmail.com>
2022-08-01 00:15:54 +0700

    sql

---
+++ database/exercise/sql-basic-of-nguyen-van-hieu/exercise-1-in-tutorial-of-nguyen-van-hieu.sql
@@ -0,0 +1,16 @@
+-- create database nguyen_van_hieu
+-- drop table nguyen_van_hieu.exercise1;
+
+-- create table nguyen_van_hieu.table_persion_1 (
+-- 	name_human varchar(30),
+--     continent varchar(30),
+--     erea int, 
+--     population int,
+--     gdb int
+-- );
+
+
+insert into nguyen_van_hieu.table_persion_1 (name_human, continent, erea, population, gdb)
+values (Afghanistan, Asia, 652230, 25500100, 20343000000);
+
+select * from nguyen_van_hieu.table_persion_1 ;
